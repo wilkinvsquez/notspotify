e7f02fa6386e108c6ada1783847c046bc9d8d487
wilkinvsquez <wilkinvsquez@gmail.com>
2025-05-07 22:39:43 -0600

    test

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/notspotify/',
   plugins: [
     react(),
     tailwindcss(),

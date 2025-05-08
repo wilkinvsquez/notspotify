178b1448a1b921b6f81a98befa434af0110fc772
wilkinvsquez <wilkinvsquez@gmail.com>
2025-05-07 22:27:12 -0600

    404 page

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,13 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
 
+const repoName = 'notspotify';
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),],
+  base: `/${repoName}/`,
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
 })
